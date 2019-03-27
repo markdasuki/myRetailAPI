71b58b173c726dfaa25c802f4842ede9e82a77a6
Dasuki <Mark.Dasuki@Emerson.com>
2019-03-26 22:15:39 -0500

    Removed logger and changed some variable names

--- src/main/java/com/myRetail/myRetailAPI/service/RedskyClientService.java
+++ src/main/java/com/myRetail/myRetailAPI/service/RedskyClientService.java
@@ -2,8 +2,6 @@ package com.myRetail.myRetailAPI.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +14,6 @@ public class RedskyClientService {
 
     private final String redskyUrl = "https://redsky.target.com/v2/pdp/tcin/";
     private final String redskyUrlExclusion = "?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics";
-    Logger logger = LoggerFactory.getLogger(RedskyClientService.class);
 
     /*
     * Retrieve product title for a given product from the Redsky api
@@ -26,27 +23,29 @@ public class RedskyClientService {
     *
     * */
     public String getProductTitleFromRedsky(int productId) throws Exception {
-        logger.info("getProductTitleFromRedsky");
         String productTitle;
         RestTemplate restTemplate = new RestTemplate();
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode root;
+        JsonNode jsonResponse;
 
         String urlFromProductId =  createURLFromProductId(productId, redskyUrl, redskyUrlExclusion);
 
         //Perform get request
-        ResponseEntity<String> response = restTemplate.getForEntity(urlFromProductId, String.class);
-        root = mapper.readTree(response.getBody());
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(urlFromProductId, String.class);
+        jsonResponse = mapper.readTree(getResponse.getBody());
 
         //Retrieve title value from
-        productTitle = root.get("product").get("item").get("product_description").get("title").textValue();
+        productTitle = jsonResponse.get("product")
+                           .get("item")
+                           .get("product_description")
+                           .get("title")
+                           .textValue();
 
         return productTitle;
-
     }
 
     /*
-    * Creates a URL to perform the GET request for a product id
+    * Creates URL to perform the GET request for a product id
     *
     * @param productId The product id that will be attached to the URL
     * @param url The url that needs to be modified
