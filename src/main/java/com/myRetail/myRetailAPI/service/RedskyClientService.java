77825f84364ed1b1a669b8b4d925f678830a541f
Dasuki <Mark.Dasuki@Emerson.com>
2019-03-27 10:55:42 -0500

    Minor comment change to RedSkyClient

--- src/main/java/com/myRetail/myRetailAPI/service/RedskyClientService.java
+++ src/main/java/com/myRetail/myRetailAPI/service/RedskyClientService.java
@@ -19,7 +19,7 @@ public class RedskyClientService {
     * Retrieve product title for a given product from the Redsky api
     *
     * @param productId The id of the product that is requesting the title
-    * @return string of title
+    * @return string of product title
     *
     * */
     public String getProductTitleFromRedsky(int productId) throws Exception {
@@ -50,7 +50,7 @@ public class RedskyClientService {
     * @param productId The product id that will be attached to the URL
     * @param url The url that needs to be modified
     * @param urlExclusion Exclusions from the api request
-    *
+    * @return string of URL
     *
     * */
     public String createURLFromProductId(int productId, String url, String urlExclusion) {
